43c5d8e77eb624da3fb5f6029f3bc12813851fdf
lgautier <lgautier@localhost>
2010-05-26 08:21:46 +0200

    version number to be 2.1.3

--- rpy/__init__.py
+++ rpy/__init__.py
@@ -1,4 +1,4 @@
-__version_vector__ = ((2,1,2), '')
+__version_vector__ = ((2,1,3), '')
 
 __version__ = '.'.join([str(x) for x in __version_vector__[0]]) + \
               '' + __version_vector__[1]
