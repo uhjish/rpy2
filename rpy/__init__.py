afa73f65b902decbc9b90327b0cc396c8a10e3b8
lgautier <lgautier@localhost>
2010-11-27 11:19:36 +0100

    - fixed rpy2.robjects.R to be a singleton - edited NEWS entries - version number to 2.1.9

--- rpy/__init__.py
+++ rpy/__init__.py
@@ -1,4 +1,4 @@
-__version_vector__ = ((2,1,8), '')
+__version_vector__ = ((2,1,9), '')
 
 __version__ = '.'.join([str(x) for x in __version_vector__[0]]) + \
               '' + __version_vector__[1]
