7205a87a1edd37249de4f22d9224065a3d27409e
Laurent Gautier <lgautier@gmail.com>
2013-09-28 11:43:07 -0400

    Version bump. Getting ready for bugfix release.

--- rpy/__init__.py
+++ rpy/__init__.py
@@ -1,5 +1,5 @@
 
-__version_vector__ = ((2,3,7), '')
+__version_vector__ = ((2,3,8), '')
 
 __version__ = '.'.join([str(x) for x in __version_vector__[0]]) + \
               '' + __version_vector__[1]
