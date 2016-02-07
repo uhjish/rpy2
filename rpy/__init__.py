f889b2797348e4b4e044f246e02c258f09a74b3d
Laurent Gautier <lgautier@gmail.com>
2016-02-07 13:53:33 -0500

    Bump version number to 2.7.9

--- rpy/__init__.py
+++ rpy/__init__.py
@@ -1,5 +1,5 @@
 
-__version_vector__ = ((2,7,8), '')
+__version_vector__ = ((2,7,9), '')
 
 __version__ = '.'.join([str(x) for x in __version_vector__[0]]) + \
               '' + __version_vector__[1]
